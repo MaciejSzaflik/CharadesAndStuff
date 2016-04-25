d38dde244f539486ad52e34e44790ef668bdd8e0
Marcin Wincek <mwincek005@gmail.com>
2016-04-25 22:32:06 +0200

    Include material-ui

--- public/src/components/Home.js
+++ public/src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import RaisedButton from 'material-ui/lib/raised-button';
 
 export class Home extends React.Component {
   render() {
     return (
       <div className="mdl-card mdl-shadow--2dp">
-        <RaisedButton>aaa</RaisedButton>
         qweqweddasdasddd
       </div>
     )
--- public/src/components/Main.js
+++ public/src/components/Main.js
@@ -2,27 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import {AppBar, Tabs, Tab} from 'material-ui';
 import {Grid, Col, Row} from 'react-flexbox-grid';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import {Navbar} from './Navbar';
 import styles from '../styles/main.css';
 
-export class Main extends React.Component {
-  render() {
-    let links = [{
-      path: '/',
-      name: 'Main'
-    }, {
-      path: '/home',
-      name: 'Home'
-    }];
-    return (
-      <div>
-        <AppBar title="Charades and Stuff"></AppBar>
-        <Grid>
-          <Row>
-          </Row>
-        </Grid>
-      </div>
-    )
-  }
-}
+export const Main = function() {
+  return (
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <AppBar title="Charades and Stuff"></AppBar>
+    </MuiThemeProvider>
+  )
+};
