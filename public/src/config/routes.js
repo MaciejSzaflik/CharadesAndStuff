626dc4c89e69cfe8449fd60df210ccc7b3fd3752
Marcin Wincek <mwincek005@gmail.com>
2016-05-07 14:35:15 +0200

    Main page proposition

--- public/src/config/routes.js
+++ public/src/config/routes.js
@@ -3,11 +3,12 @@ import {Router, Route, IndexRoute} from 'react-router';
 import {Main} from '../main';
 import {Puns} from '../puns';
 import {Checkers} from '../checkers';
-import {Home} from '../components/Home';
+import {Home} from '../home';
 
 export let routes = (
   <Router>
     <Route path="/" component={Main}>
+      <IndexRoute component={Home} />
       <Route path="/puns" component={Puns} />
       <Route path="/checkers" component={Checkers} />
     </Route>
