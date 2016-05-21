5fedec8ff5a22da3f44f016c64dd1bba55e88200
mikolajswiatek <mikolajswiatek@outlook.com>
2016-05-21 13:19:50 +0200

    Added event for button create room

--- app/assets/javascripts/lobby/lobby.js
+++ app/assets/javascripts/lobby/lobby.js
@@ -1,16 +1,30 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    var lobby = new Lobby();
+var CREATE_ROOM_URL = "http://localhost:9000/room/create/";
 
+$(document).ready(function() {
+    console.log( "ready!" );
+    var url = document.URL.split("/");
+    var game = url[url.length - 1];
+    var isValid = false;
+
+    if (game == "stuff" || game == "charades") {
+        isValid = true;
+    }
+
+    var lobby = new Lobby(game, isValid);
     var button = document.getElementById("create-room");
     button.addEventListener('click', function() {
-        lobby.addRoom("");
+        lobby.addRoom();
     }, false);
 
 });
 
-function Lobby() {
-    this.addRoom = function(name) {
-        console.log("ll");
+function Lobby(game, isValid) {
+    this.game = game;
+    this.isValid = isValid;
+
+    this.addRoom = function() {
+        $.get(CREATE_ROOM_URL + game, function(data) {
+            location.reload();
+        });
     }
 }
