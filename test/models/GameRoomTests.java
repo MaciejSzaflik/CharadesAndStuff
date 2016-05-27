88d1bac78739012f7faa94814b2099a292462330
Mikołaj Świątek <Mikoaj witek>
2016-05-27 19:25:31 +0200

    Added Simple Model test

---
+++ test/models/GameRoomTests.java
@@ -0,0 +1,42 @@
+package test.models;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import models.Gamer;
+import models.Room;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+import play.test.*;
+import static play.test.Helpers.*;
+
+public class GameRoomTests {
+    @Test
+    public void GameRoomTests_AddRoom() {
+        running(fakeApplication(), new Runnable() {
+            public void run() {
+              Room room = getRoom();
+              room.save();
+              
+              Assert.assertNotNull(room.id);
+            }
+          });
+    }
+    
+    private Room getRoom() {
+        Room room = new Room();
+        room.dateCreation = new Date();
+        room.dateUpdate = new Date();
+        room.iStuff = true;
+        room.isRunning = false;
+        room.gameId = new Long(1);
+        room.chatId = new Long(1);
+        room.params = "";
+    	room.players = new ArrayList<Gamer>();
+    	
+    	return room;
+    }
+}
